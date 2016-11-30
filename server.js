8f62599cf6497fc7c5eb778108f803a611aec15d
aceineights <blacksam@oregonstate.edu>
2016-11-30 10:51:16 -0800

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ mysqlConnection.query('SELECT * FROM Locations ORDER BY Distance DESC', function
         longitude: row.Longitude,
         discription: row.Discription,
         distance: row.Distance
-      }console.log("Sorted"));
+      )}console.log("Sorted"));
       
 /*
  * Set up Express to use express-handlebars as the view engine.  This means
